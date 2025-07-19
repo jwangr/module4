22874f5ad2235d9266b627eb107e01a3f14554e9
jwangr <jwanggr@gmail.com>
2025-07-19 14:52:44 +1000

    Added filtered categories

--- e-commerce/js/main.js
+++ e-commerce/js/main.js
@@ -4,8 +4,16 @@ window.onload = async () => {
         const products = await productsDao.getProducts();
         console.log(products);
         products.forEach(element => {
+            getCategories(element);
             displayProducts(element);
         });
+        displayCategories();
+
+        document.getElementById("categorySelection").addEventListener("change", () => {
+            let option = document.getElementById("categorySelection").value;
+            filterProducts(option);
+            console.log(option);
+        })
     }
     catch (error) {
         console.log(error)
@@ -20,4 +28,36 @@ function displayProducts(product) {
     template.querySelector('.image').style.backgroundImage = `url(${product.image})`;
 
     document.getElementById("productspanel").appendChild(template);
+}
+
+let categories = new Set();
+function getCategories(element) {
+    categories.add(element.category);
+}
+
+function displayCategories() {
+    const categoryList = Array.from(categories).sort();
+    // categoryList.map(category => category.slice(0,1).toUpperCase())
+    console.log(categoryList);
+    const selector = document.getElementById("categorySelection");
+
+    categoryList.forEach(element => {
+        const option = document.createElement("option");
+        option.value = element;
+        option.innerText = element.charAt(0).toUpperCase() + element.slice(1);
+
+        selector.appendChild(option);
+    });
+}
+
+async function filterProducts(category) {
+    document.querySelector("#productspanel").innerHTML = "";
+    try {
+        const productsDao = new ProductsDao();
+        const items = await productsDao.getProducts();
+        items.forEach(element => {
+            if (element.category == category) { displayProducts(element) };
+        });
+    }
+    catch (error) {console.log(error)};
 }
